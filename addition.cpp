52bed34d13224e9a2ccfaeed52151d1785b7af33
root <root@localhost.localdomain>
2021-06-04 18:34:36 +0530

    adding c++ program

---
+++ addition.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	 int firstnum =8;
+	int secondnum =9;
+	int sum = firstnum + secondnum;   
+    
+    cout<<"Result : "; << sum ;
+    cin>>firstNumber >> secondNumber;
+	return 0;
+}
