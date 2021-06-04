3dda6f69476269e9a1aa4101b728e15ddd1dff56
root <root@localhost.localdomain>
2021-06-04 17:28:29 +0530

    adding userinfo.sh and minage.csv

--- userinfo.sh
+++ userinfo.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+echo "username,  minage " > minage.csv
+for user in $(cat /etc/passwd | grep bash | awk -F ':' '{ print $1 }')
+do
+	echo "$user,";chage -I $user | grep "Minimun" | sed 's/Minimum number of days between passwd change   ://g'
+done | paste-->>minage.csv
